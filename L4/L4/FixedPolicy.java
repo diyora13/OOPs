cbc073b91a59d08366bbafa8deb28a520ad60539
Manan Diyora <60748491+diyora13@users.noreply.github.com>
2026-01-22 17:38:02 +0530

    Add FixedPolicy class with show method

---
+++ L4/L4/FixedPolicy.java
@@ -0,0 +1,10 @@
+package L4;
+
+// final class demo (cannot be extended)
+final public class FixedPolicy {
+    public void show() {
+        System.out.println("Policy active");
+    }
+}
+// class ExtendPolicy extends FixedPolicy { } //  not allowed
+
